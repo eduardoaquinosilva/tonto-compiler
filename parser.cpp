b15708c99ed528f46b20217790204deed0a2a920
Rafael Neves <rafaelneves0405@gmail.com>
2025-10-17 01:12:40 -0300

    feat: adicionado retorno em arquivos

--- parser.cpp
+++ parser.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "parser.h"
 #include "tokens.h"
 using namespace std;
 
 int columnNumber = 1;
 int keyWordsCount = 0;
+int classCount = 0;
+int relationsCount = 0;
+int instanceCount = 0;
+int classStereotypesCount = 0;
+int relationsStereotypesCount = 0;
+int metaAttributesCount = 0;
+int typesCount = 0;
+int newTypesCount = 0;
+int specialSymbolsCount = 0;
 
 char **fileList = nullptr;
 unsigned nFiles = 0;
 unsigned currentFile = 0;
 bool openFile = false;
 ifstream fin;
+stringstream ss;
 
 Parser::Parser(char** list, unsigned count) : lookahead(0) 
 {
@@ -26,7 +37,6 @@ int yyFlexLexer::yywrap()
 		openFile = false;
 		fin.close();
 	}
-
 	while (!openFile && (currentFile < nFiles))
     {
         fin.open(fileList[currentFile]); 
@@ -50,13 +60,15 @@ void Parser::Start()
 {
     scanner.switch_streams(&fin);
 
+    
+
     if (scanner.yywrap() == 1) {
         cout << "No input files provided or all files failed to open.\n";
         return;
     }
 
-    cout << "Token\t\tLinha\tColuna\n";
-    cout << "-------------------------------\n";
+    ss << "Token\t\tLinha\tColuna\n";
+    ss << "-------------------------------\n";
 
     // enquanto não atingir o fim da entrada
     while ((lookahead = scanner.yylex()) != 0)
@@ -66,25 +78,38 @@ void Parser::Start()
         // trata o token recebido do analisador léxico
         switch(lookahead)
         {
-            case GENSET: cout << "<GENSET>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case DISJOINT: cout << "<DISJOINT>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case COMPLETE: cout << "<COMPLETE>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case GENERAL: cout << "<GENERAL>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case SPECIFICS: cout << "<SPECIFICS>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case WHERE: cout << "<WHERE>\t\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case PACKAGE: cout << "<PACKAGE>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case IMPORT: cout << "<IMPORT>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
-            case FUNCTIONAL_COMPLEXES: cout << "<FUNCTIONAL_COMPLEXES>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case GENSET: ss << "<GENSET>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case DISJOINT: ss << "<DISJOINT>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case COMPLETE: ss << "<COMPLETE>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case GENERAL: ss << "<GENERAL>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case SPECIFICS: ss << "<SPECIFICS>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case WHERE: ss << "<WHERE>\t\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case PACKAGE: ss << "<PACKAGE>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case IMPORT: ss << "<IMPORT>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
+            case FUNCTIONAL_COMPLEXES: ss << "<FUNCTIONAL_COMPLEXES>\t" << scanner.lineno() << "\t" << tokenStartColumn << "\n"; break;
             
-            case TYPE: cout << "token type " << scanner.YYText() << '\n'; break;
-            case NEW_TYPE: cout << "token new_type " << scanner.YYText() << '\n'; break;
-            case INSTANCE_NAME: cout << "token name_instance " << scanner.YYText() << '\n'; break;
-            case CLASS_NAME: cout << "token name_class " << scanner.YYText() << '\n'; break;
-            case RELATION_NAME: cout << "token name_relation " << scanner.YYText() << '\n'; break;
-            default: cout << "token " << scanner.YYText() << '\n'; break;
+            case TYPE: ss << "token type " << scanner.YYText() << '\n'; break;
+            case NEW_TYPE: ss << "token new_type " << scanner.YYText() << '\n'; break;
+            case INSTANCE_NAME: ss << "token name_instance " << scanner.YYText() << '\n'; break;
+            case CLASS_NAME: ss << "token name_class " << scanner.YYText() << '\n'; break;
+            case RELATION_NAME: ss << "token name_relation " << scanner.YYText() << '\n'; break;
+            default: ss << "token " << scanner.YYText() << '\n'; break;
         }
+
+    }
+    
+
+    ss << "-------------------------------\n";
+    ss << "Total de palavras reservadas: " << keyWordsCount << "\n";
+
+    std::ofstream file("../output.txt");
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file.\n";
     }
 
-    cout << "-------------------------------\n";
-    cout << "Total de palavras reservadas: " << keyWordsCount << "\n";
+    file << ss.str();
+
+    file.close();
+
+    std::cout << "Data written to output.txt\n";
 }
